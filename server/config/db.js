1bba1793c54da4b5c98c430f85b799943fc5d780
Gokulrajv1304 <gvenugo1@my.centennialcollege.ca>
2022-02-21 21:47:09 -0500

    updated URI with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://0.0.0.0:27017/WebAppDev"
-    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI": "mongodb://0.0.0.0:27017/WebAppDev"
+    "URI": "mongodb+srv://Gokulraj:EBdMavrqiB8yYa9M@mongodbserver.is4xt.mongodb.net/WebAppDev?retryWrites=true&w=majority"
     //"Secret": 'SomeSecret'
 }
